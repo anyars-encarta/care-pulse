2f5300c401f72c28a9a3503dff0b786a651557c6
anyars-encarta <anyarsencarta@gmail.com>
2024-07-08 14:18:34 +0000

    Install shadcn-ui@latest add form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
 
 const PatientForm = () => {
   return (
