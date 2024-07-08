3344df0a6a954a8710c35b42472b84080d70e80c
anyars-encarta <anyarsencarta@gmail.com>
2024-07-08 12:06:59 +0000

    install next-themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div>
       <h1 className='text-3xl underline text-white'>Home</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
