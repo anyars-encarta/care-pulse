a991aa57a656ae2ac34092cffad404a3c2c94b92
anyars-encarta <anyarsencarta@gmail.com>
2024-07-09 13:28:55 +0000

    Install node-appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -22,6 +23,7 @@ export enum FormFieldType {
 }
 
 const PatientForm = () => {
+    const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
 
     const form = useForm<z.infer<typeof UserFormValidation>>({
@@ -33,9 +35,21 @@ const PatientForm = () => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof UserFormValidation>) {
-        console.log(values)
+    const onSubmit = async ({ name, email, phone }: z.infer<typeof UserFormValidation>) => {
+        setIsLoading(true);
+
+        try {
+            // const userData = { name, email, phone };
+
+            // const user = await createUser(userData);
+
+            // if (user) router.push(`/patients/${user.$id}/register`)
+
+        } catch (e) {
+            console.log(e);
+        }
     }
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
