724cdfcc14a294d6c4d6b0744a15e5351adfcd2c
anyars-encarta <anyarsencarta@gmail.com>
2024-07-14 11:09:22 +0000

    Style file uploader field

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { convertFileToUrl } from '@/lib/utils';
+import Image from 'next/image';
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 
@@ -18,11 +20,31 @@ const FileUploader = ({ files, onChange }: FileUploaderProps) => {
     return (
         <div {...getRootProps()} className='file-upload'>
             <input {...getInputProps()} />
-            {
-                isDragActive ?
-                    <p>Drop the files here ...</p> :
-                    <p>Drag 'n' drop some files here, or click to select files</p>
-            }
+            {files && files.length > 0 ? (
+                <Image
+                    src={convertFileToUrl(files[0])}
+                    width={1000}
+                    height={1000}
+                    alt='uploaded file'
+                    className='max-h-[400px] overflow-hidden object-cover'
+                />
+            ) : (
+                <>
+                    <Image
+                        src='/assets/icons/upload.svg'
+                        width={40}
+                        height={40}
+                        alt='upload'
+                    />
+
+                    <div className='file-upload_label'>
+                        <p className='text-14-regular'>
+                            <span className='text-green-500'>Click to upload</span> or drag and drop
+                        </p>
+                        <p>SVG, PNG, JPG, JPEG, GIFF (max 800 x 400)</p>
+                    </div>
+                </>
+            )}
         </div>
     )
 };
