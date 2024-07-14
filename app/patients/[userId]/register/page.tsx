f8a0fc4d20415abc0732a65f1109c894de6e99ed
anyars-encarta <anyarsencarta@gmail.com>
2024-07-14 11:46:20 +0000

    Complete Register form

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -12,8 +12,8 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
 
     return (
         <div className='flex h-screen max-h-screen'>
-            <section className='remove-scrollbar container my-auto'>
-                <div className='sub-container max-w-[496px]'>
+            <section className='remove-scrollbar container'>
+                <div className='sub-container max-w-[860px] flex-1 flex-col py-10'>
                     <Image
                         src='/assets/icons/logo-full.svg'
                         width={1000}
@@ -24,15 +24,14 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
 
                     <RegisterForm user={user} />
 
-                    <div className='text-14-regular mt-20 flex justify-between'>
-                        <p className='justify-end text-dark-600 xl:text-left'>
-                            © 2024 CarePulse
-                        </p>
-
+                    <p className='copyright py-12'>
+                        © 2024 CarePulse
+                    </p>
+                    {/* <div className='text-14-regular mt-20 flex justify-between'>
                         <Link href='/?admin=true' className='text-green-500'>
                             Admin
                         </Link>
-                    </div>
+                    </div> */}
                 </div>
             </section>
 
