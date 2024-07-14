854f29b0f35bf561ec474c03a4c258d7e3a642ff
anyars-encarta <anyarsencarta@gmail.com>
2024-07-14 11:36:56 +0000

    Add checkboxes and labels

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -17,6 +17,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
+import { Checkbox } from "./ui/checkbox";
+import { propagateServerField } from "next/dist/server/lib/render-server";
 
 interface CustomProps {
     control: Control<any>,
@@ -132,12 +134,20 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
                     </Select>
                 </FormControl>
             )
-        
+
         case FormFieldType.CHECKBOX:
             return (
                 <FormControl>
                     <div className='flex items-center gap-4'>
-                        
+                        <Checkbox
+                            id={props.name}
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                        />
+
+                        <label htmlFor={props.name} className='checkbox-label'>
+                            {props.label}
+                        </label>
                     </div>
                 </FormControl>
             )
