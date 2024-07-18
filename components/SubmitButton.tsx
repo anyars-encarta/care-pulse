a0d55544ab28ff8c375400113721221f7908c328
anyars-encarta <anyarsencarta@gmail.com>
2024-07-18 09:57:26 +0000

    Correct SubmitButton component

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import { Button } from './ui/button'
+import Image from 'next/image';
+import { Button } from './ui/button';
 
 interface ButtonProps {
     isLoading: boolean;
@@ -9,7 +9,11 @@ interface ButtonProps {
 
 const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
     return (
-        <Button type='submit' disabled={isLoading} className={className ?? 'shad-primary-btn w-full'}>
+        <Button
+            type='submit'
+            disabled={isLoading}
+            className={className ?? 'shad-primary-btn w-full'}
+        >
             {isLoading ? (
                 <div className='flex items-center gap-4'>
                     <Image
@@ -25,7 +29,7 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
                 children
             )}
         </Button>
-    )
-}
+    );
+};
 
-export default SubmitButton
+export default SubmitButton;
