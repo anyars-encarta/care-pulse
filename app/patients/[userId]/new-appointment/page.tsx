3ae643a679bcdd08cabbf79cc6f88953fbbf6db6
anyars-encarta <anyarsencarta@gmail.com>
2024-07-19 13:19:48 +0000

    Create new appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 const NewAppointment = () => {
-  return (
-    <div>NewAppointment</div>
-  )
+    return (
+        <div className='flex h-screen max-h-screen'>
+            <section className='remove-scrollbar container my-auto'>
+                <div className='sub-container max-w-[860px] flex-1 justify-between'>
+                    <Image
+                        src='/assets/icons/logo-full.svg'
+                        width={1000}
+                        height={1000}
+                        alt="patient"
+                        className='mb-12 h-10 w-fit'
+                    />
+
+                    <AppointmentForm />
+                    
+                    <p className='justify-end text-dark-600 xl:text-left'>
+                        © 2024 CarePulse
+                    </p>
+                </div>
+            </section>
+
+            <Image
+                src='/assets/images/appointment-img.png'
+                height={1000}
+                width={1000}
+                alt='appointment'
+                className='side-img max-w-[390px] bg-bottom'
+            />
+        </div>
+    );
 }
 
-export default NewAppointment
+export default NewAppointment;
