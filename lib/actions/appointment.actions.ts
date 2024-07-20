b8c0ed848cb5c72319476d1d11b9a8a9dfbdf9a2
anyars-encarta <anyarsencarta@gmail.com>
2024-07-20 11:49:39 +0000

    Implement success-page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -18,3 +18,21 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
         console.log(e)
     }
 }
+
+// GET APPOINTMENT
+export const getAppointment = async (appointmentId: string) => {
+    try {
+      const appointment = await databases.getDocument(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        appointmentId
+      );
+  
+      return parseStringify(appointment);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the existing patient:",
+        error
+      );
+    }
+  };
