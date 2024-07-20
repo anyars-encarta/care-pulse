8b02475e869e977da0a8c559fc81780e00cdbc20
anyars-encarta <anyarsencarta@gmail.com>
2024-07-20 19:18:27 +0000

    Use database data in Cards

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
 import StatCard from '@/components/StatCard'
+import { getRecentAppointmentsList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
-const Admin = () => {
+const Admin = async () => {
+    const appointments = await getRecentAppointmentsList()
+
+    console.log(appointments)
     return (
         <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
             <header className='admin-header'>
@@ -29,21 +32,21 @@ const Admin = () => {
                 <section className='admin-stat'>
                     <StatCard
                         type='appointments'
-                        count={5}
-                        label='Scgeduled Appointments'
+                        count={appointments.scheduledCount}
+                        label='Scheduled Appointments'
                         icon='/assets/icons/appointments.svg'
                     />
 
                     <StatCard
                         type='pending'
-                        count={10}
+                        count={appointments.pendingCount}
                         label='Pending Appointments'
                         icon='/assets/icons/pending.svg'
                     />
 
                     <StatCard
                         type='cancelled'
-                        count={2}
+                        count={appointments.cancelledCount}
                         label='Cancelled Appointments'
                         icon='/assets/icons/cancelled.svg'
                     />
