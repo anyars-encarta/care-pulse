066f57e2d8ebb3dbce9a1a0dc7c387d453063c58
anyars-encarta <anyarsencarta@gmail.com>
2024-07-20 14:36:40 +0000

    Open open dialog from Admin button and close from close button

--- components/ui/PassKeyModal.tsx
+++ components/ui/PassKeyModal.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     AlertDialog,
     AlertDialogAction,
@@ -9,19 +11,42 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const PassKeyModal = () => {
+    const router = useRouter();
+    const [open, setOpen] = useState(true);
+
+    const closeModal = () => {
+        setOpen(false);
+        router.push('/')
+    };
+
     return (
-        <AlertDialog>
-            <AlertDialogTrigger>Open</AlertDialogTrigger>
-            <AlertDialogContent>
+        <AlertDialog open={open} onOpenChange={setOpen}>
+            <AlertDialogContent className='shad-alert-dialog'>
                 <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogTitle className='flex items-start justify-between'>
+                        Admin Access Verification
+                        <Image
+                            src='/assets/icons/close.svg'
+                            height={20}
+                            width={20}
+                            alt='close'
+                            onClick={() => closeModal()}
+                            className='cursor-pointer'
+                        />
+                    </AlertDialogTitle>
                     <AlertDialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
+                        To access the admin page, please enter the passKey.
                     </AlertDialogDescription>
                 </AlertDialogHeader>
+
+                <div>
+                    
+                </div>
                 <AlertDialogFooter>
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
                     <AlertDialogAction>Continue</AlertDialogAction>
