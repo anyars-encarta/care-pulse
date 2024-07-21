ae2de34539c3083de4518c4a9ca410ee36d0c7f0
anyars-encarta <anyarsencarta@gmail.com>
2024-07-21 19:24:41 +0000

    Add SMS Notification

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,8 @@
 'use server'
 
 import { ID, Query } from "node-appwrite"
-import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config"
-import { parseStringify } from "../utils";
+import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, messaging } from "../appwrite.config"
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -42,7 +42,7 @@ export const getAppointment = async (appointmentId: string) => {
 // GET RECENT APPOINTMENTS
 export const getRecentAppointmentsList = async () => {
   try {
-    const appointments  = await databases.listDocuments(
+    const appointments = await databases.listDocuments(
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
       [Query.orderDesc('$createdAt')]
@@ -78,7 +78,7 @@ export const getRecentAppointmentsList = async () => {
   }
 }
 
-export const updateAppointment = async ({ appointmentId, userId, appointment, type}: UpdateAppointmentParams) => {
+export const updateAppointment = async ({ appointmentId, userId, appointment, type }: UpdateAppointmentParams) => {
   try {
     const updatedAppointment = await databases.updateDocument(
       DATABASE_ID!,
@@ -91,10 +91,32 @@ export const updateAppointment = async ({ appointmentId, userId, appointment, ty
       throw new Error('Appointment not found')
     }
 
-    // TODO SMS Confirmation
+    const smsMessage = `
+    Hi, it's CarePulse. 
+    ${type === 'schedule'
+        ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}.`
+        : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}.`
+      }
+    `
+      await sendSMSNotification(userId, smsMessage);
 
     revalidatePath('/admin')
     return parseStringify(updatedAppointment)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    )
+
+    return parseStringify(message);
   } catch (e) {
     console.log(e)
   }
